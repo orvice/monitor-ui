047b90371a03a0e8d2c69a146a40f746e30239a3
orvice <orvice@gmail.com>
2018-03-17 23:02:19 +0800

    fix

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -n $API_URI ]; then
-  sed -i "s|http://127.0.0.1:2008|API_URI|g" /usr/share/nginx/html/static/js/*.js
+  sed -i "s|http://127.0.0.1:2008|$API_URI|g" /usr/share/nginx/html/static/js/*.js
 else
   echo "no replace api_uri"
 fi
